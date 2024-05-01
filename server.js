5c27fd647777f060d87f016c66144dbca668ca91
kenpachit <161153415+kenpachit@users.noreply.github.com>
2024-05-01 17:01:45 +0200

    cleaning code

--- server.js
+++ server.js
@@ -5,16 +5,16 @@ const cors = require('cors');
 const morgan = require('morgan');
 
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 const MONGODB_URI = process.env.MONGODB_URI;
 
-app.use(express.json());
+app.use(express.json()); 
+app.use(cors()); 
+app.use(morgan('common')); 
 
-app.use(cors());
-
-app.use(morgan('common'));
-
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose
+  .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connection successful'))
   .catch((error) => console.error('MongoDB connection error:', error));
 
@@ -23,8 +23,8 @@ app.get('/api/sample', (req, res) => {
 });
 
 app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
+  console.error(err.stack); 
+  res.status(500).send('Something broke!'); 
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
