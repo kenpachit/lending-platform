afe2f96a5cde98f2b467e760ce3d5ee3700a92b0
kenpachit <161153415+kenpachit@users.noreply.github.com>
2024-05-02 16:24:43 +0200

    function added

--- itemController.js
+++ itemController.js
@@ -2,22 +2,38 @@ const mongoose = require('mongoose');
 const ItemModel = require('./models/Item');
 const NodeCache = require('node-cache');
 const itemCache = new NodeCache({ stdTTL: 100, checkperiod: 120 });
+const EventEmitter = require('events');
+class MyEmitter extends EventEmitter {};
+const myEmitter = new MyEmitter();
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
-const addItem = async (itemDetails) => { 
+myEmitter.on('itemAdded', (item) => {
+  console.log(`New item added: ${item.name}`);
+});
+
+myEmitter.on('itemUpdated', (item) => {
+  console.log(`Item updated: ${item.name}`);
+});
+
+myEmitter.on('itemRemoved', (itemId) => {
+  console.log(`Item removed with ID: ${itemId}`);
+});
+
+const addItem = async (itemDetails) => {
   if (!itemDetails.name || !itemDetails.price) {
     throw new Error('Missing required fields');
   }
   const newItem = new ItemModel(itemDetails);
   const savedItem = await newItem.save();
   itemCache.del("allItemsCache");
+  myEmitter.emit('itemAdded', savedItem);
   return savedItem;
 };
 
-const fetchItemById = async (itemId) => { 
+const fetchItemById = async (itemId) => {
   const cacheKey = `item_${itemId}`;
   const cachedItem = itemCache.get(cacheKey);
   if (cachedItem) {
@@ -31,7 +47,7 @@ const fetchItemById = async (itemId) => {
   return item;
 };
 
-const fetchAllItems = async () => { 
+const fetchAllItems = async () => {
   const allItemsCacheKey = "allItemsCache";
   const cachedItems = itemCache.get(allItemsCacheKey);
   if (cachedItems) {
@@ -42,7 +58,7 @@ const fetchAllItems = async () => {
   return items;
 };
 
-const updateItemDetails = async (itemId, newData) => { 
+const updateItemDetails = async (itemId, newData) => {
   const cacheKey = `item_${itemId}`;
   const updatedItem = await ItemModel.findByIdAndUpdate(itemId, newData, { new: true });
   if (!updatedItem) {
@@ -50,6 +66,7 @@ const updateItemDetails = async (itemId, newData) => {
   }
   itemCache.set(cacheKey, updatedItem);
   itemCache.del("allItemsCache");
+  myEmitter.emit('itemUpdated', updatedItem);
   return updatedItem;
 };
 
@@ -61,6 +78,7 @@ const removeItem = async (itemId) => {
   }
   itemCache.del(cacheKey);
   itemCache.del("allItemsCache");
+  myEmitter.emit('itemRemoved', itemId);
   return deletedItem;
 };
 
